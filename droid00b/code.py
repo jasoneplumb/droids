a4d1fc72105c2484b6a57df48dd21ddcd4e9a0cd
Jason Plumb <jasoneplumb@gmail.com>
2022-09-11 14:03:36 -0700

    reverted to original sensors/wiring

--- droid00b/code.py
+++ droid00b/code.py
@@ -1,9 +1,68 @@
 # droid00b: periodic upload of local greenhouse sensor data
-# TODO test new barometer
 # TODO factor out, as a function, the multi-pass try/catch pattern
 # TODO factor out, as a file, each sensor's logic
 # TODO Add support for sensor specific sampling frequencies
-MINUTES_BETWEEN_SAMPLES = 1
+
+droid = {
+    'description': {
+        'sensors': [{
+            'pretty_name': 'Relative Humidity', 
+            'endpoint': 'proto/sensor_droid/relative_humidity', 
+            'reading_column_name': 'relative_humidity',
+            'reading_units': 'Percentage', 
+            'period': '3', 
+            'period_units': 'hours',
+            'precision': '0.1', 
+            'accuracy': '0.01'
+        }, {
+            'pretty_name': 'Wi-Fi Signal Strength', 
+            'endpoint': 'proto/sensor_droid/rssi', 
+            'reading_column_name': 'rssi',
+            'reading_units': 'RSSI', 
+            'period': '1', 
+            'period_units': 'days',
+            'precision': '1.0', 
+            'accuracy': '0.1'
+        }, {
+            'pretty_name': 'Soil Moisture', 
+            'endpoint': 'proto/sensor_droid/soil_moisture', 
+            'reading_column_name': 'soil_moisture',
+            'reading_units': 'Percentage', 
+            'period': '3', 
+            'period_units': 'hours',
+            'precision': '1.0', 
+            'accuracy': '5.0'
+        }, {
+            'pretty_name': 'Soil Temperature', 
+            'endpoint': 'proto/sensor_droid/soil_temperature', 
+            'reading_column_name': 'soil_temperature',
+            'reading_units': 'Celcius', 
+            'period': '1', 
+            'period_units': 'hours',
+            'precision': '0.01', 
+            'accuracy': '2.0'
+        }, {
+            'pretty_name': 'Ambient Temperature', 
+            'endpoint': 'proto/sensor_droid/temperature', 
+            'reading_column_name': 'temperature',
+            'reading_units': 'Celcius', 
+            'period': '30', 
+            'period_units': 'minutes',
+            'precision': '0.1', 
+            'accuracy': '0.01'
+        }, {
+            'pretty_name': 'Ultraviolet Radiation', 
+            'endpoint': 'proto/sensor_droid/uv', 
+            'reading_column_name': 'uv_index',
+            'reading_units': 'UV Index', 
+            'period': '3', 
+            'period_units': 'hours',
+            'precision': '0.1', 
+            'accuracy': '0.01'
+        }]
+    }
+}
+
 
 # Indicate that we are starting up   
 import board
@@ -51,42 +110,65 @@ i2csoil.unlock()
 if soil_sensor_found:
     from adafruit_seesaw.seesaw import Seesaw 
     ss = Seesaw(i2csoil, addr=0x36)
-    # Read sensor values and add sensor bias
-    soil_moisture = ss.moisture_read() + secrets['soil_moisture']
+    # Read sensor values and convert to RH%
+    soil_moisture = ss.moisture_read() / 20
     time.sleep(0.2) # sensors cannot be read back to back, need small delay
-    soil_temperature = ss.get_temp() + secrets['soil_temperature']
+    soil_temperature = ss.get_temp()
     print("soil_moisture: " + str(soil_moisture))
     print("soil_temperature: " + str(soil_temperature))
 
 from analogio import AnalogIn
 def get_voltage(pin):
     return (pin.value * 3.3) / 65536
-uv_index = (get_voltage(AnalogIn(board.A3)) * 10) + secrets['uv_index']
+uv_index = (get_voltage(AnalogIn(board.A3)) * 10)
 print("uv_index: " + str(uv_index))
 
 relative_humidity = None
 temperature = None
-from adafruit_bme280 import basic as adafruit_bme280
-failure_count = 0
-success = False
-while not success:
-    try:
-        i2c = board.I2C()  # uses board.SCL and board.SDA
-        bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-        relative_humidity = bme280.humidity + secrets['relative_humidity']
-        temperature = bme280.temperature + secrets['temperature']
-        success = True
-    except Exception as error:
-        failure_count += 1
-        if failure_count >= 3:
-            print("Failed to read am2320 (" + str(error) + ")")
-            break
-        time.sleep(5)
-        continue
+USE_BME280 = False
+if (USE_BME280):
+    from adafruit_bme280 import basic as adafruit_bme280
+    failure_count = 0
+    success = False
+    while not success:
+        try:
+            i2c = board.I2C()  # uses board.SCL and board.SDA
+            bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+            relative_humidity = bme280.humidity
+            temperature = bme280.temperature
+            success = True
+        except Exception as error:
+            failure_count += 1
+            if failure_count >= 3:
+                print("Failed to read bme280 (" + str(error) + ")")
+                break
+            time.sleep(5)
+            continue
+else:
+    from adafruit_am2320 import AM2320
+    i2c = bitbangio.I2C(board.D12, board.D11)
+    failure_count = 0
+    success = False
+    while not success:
+        try:
+            sensor = AM2320(i2c)
+            time.sleep(1)
+            temperature = sensor.temperature
+            time.sleep(1)
+            relative_humidity = sensor.relative_humidity
+            success = True
+        except Exception as error:
+            failure_count += 1
+            if failure_count >= 3:
+                print("Failed to read am2320 (" + str(error) + ")")
+                break
+            time.sleep(5)
+            continue
 if (relative_humidity != None): print("relative_humidity: " + str(relative_humidity))
 if (temperature != None): print("temperature: " + str(temperature))
 
 # Attach to the network
+MINUTES_BETWEEN_RETRIES = 1
 BLUE = (0, 0, 5)
 neopixel.fill(BLUE)
 from digitalio import DigitalInOut
@@ -110,7 +192,7 @@ while not esp.is_connected:
         failure_count += 1
         if failure_count >= 3:
             print("Error: Failed to get connect (" + str(error) + ")")
-            deep_sleep_then_restart(MINUTES_BETWEEN_SAMPLES)
+            deep_sleep_then_restart(MINUTES_BETWEEN_RETRIES)
         time.sleep(5)
         print("Warning: Retrying AP connection!")
         continue
@@ -136,7 +218,7 @@ while not UTC_response:
         failure_count += 1
         if failure_count >= 3:
             print("Failed to get UTC (" + str(error) + ")")
-            deep_sleep_then_restart(MINUTES_BETWEEN_SAMPLES)
+            deep_sleep_then_restart(MINUTES_BETWEEN_RETRIES)
         print("Warning: Retrying UTC request!")
         time.sleep(5)
         continue
@@ -166,7 +248,7 @@ def upload(sensor_group_name, sensor_name, sensor_value, ts):
             failure_count += 1
             if failure_count >= 3:
                 print("Failed to upload sensor data (" + str(error) + ")")
-                deep_sleep_then_restart(MINUTES_BETWEEN_SAMPLES)
+                deep_sleep_then_restart(MINUTES_BETWEEN_RETRIES)
             print("Warning: Retrying data upload!")
             time.sleep(5)
             continue
@@ -180,10 +262,10 @@ upload("uv", "uv_index", str(uv_index), UTC)
 if (relative_humidity != None): upload("relative_humidity", "relative_humidity", str(relative_humidity), UTC)
 if (temperature != None): upload("temperature", "temperature", str(temperature), UTC)
 if (soil_sensor_found):
-    upload("soil_temperature", "temp_temperature", str(soil_temperature), UTC)
+    upload("soil_temperature", "soil_temperature", str(soil_temperature), UTC)
     upload("soil_moisture", "soil_moisture", str(soil_moisture), UTC)
 
 # shutdown
 esp.disconnect()
 print("Disconnected from", secrets['ssid'])
-deep_sleep_then_restart(MINUTES_BETWEEN_SAMPLES)
+deep_sleep_then_restart(MINUTES_BETWEEN_RETRIES)
