2df9891cc125a1d256dae3bfaffb017673fb9d27
Jason Plumb <jasoneplumb@gmail.com>
2022-09-13 22:25:51 -0700

    refinements

--- droid00b/secrets.py
+++ droid00b/secrets.py
@@ -2,18 +2,7 @@
 # If you put them in the code you risk committing that info or sharing it
 
 secrets = {
-    'droid_name'        : 'myDroidName',
     'droid_fk'          : ?,
-    'droid_id'          : '???',
-    'droid_purpose'     : 'What this device does, and for what purpose',
-    'soil_moisture'     : 0.0,
-    'soil_temperature'  : 0.0,
-    'uv_index'          : 0.0,
-    'relative_humidity' : 0.0,
-    'temperature'       : 0.0,
-    'user_id'           : ['TBD', '...'],
-    'email'             : 'myEmail@domain.com',
-    'timezone'          : 'America/Los_Angeles', # http://worldtimeapi.org/timezones
     'ssid'              : 'mySSID',
     'password'          : 'myPassword'
     }
