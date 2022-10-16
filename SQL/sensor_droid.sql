b1b18209cecf67de829a6e3e1070301833ff61e1
Bill Williams <divebilldive@gmail.com>
2022-10-16 14:14:23 -0700

    Fixed merge issues and one case where the foreign key was still wrong.

--- SQL/sensor_droid.sql
+++ SQL/sensor_droid.sql
@@ -1,10 +1,9 @@
-DROP DATABASE sensor_droid; 
-
+/*DROP DATABASE sensor_droid; 
+*/
 
 CREATE DATABASE IF NOT EXISTS sensor_droid;
 USE sensor_droid;
 
-/* table populated on user creation via cognito information / lambda */
 CREATE TABLE IF NOT EXISTS profiles (
 	PRIMARY KEY (id),
     id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
@@ -18,7 +17,6 @@ CREATE TABLE IF NOT EXISTS profiles (
     update_ts			TIMESTAMP			NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
-/* represents an individual droid, readings are connected to the droid */
 CREATE TABLE IF NOT EXISTS droids (
 	PRIMARY KEY (id),
     id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
@@ -32,137 +30,70 @@ CREATE TABLE IF NOT EXISTS droids (
         ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-<<<<<<< Updated upstream
-/* one soil moisture reading */
+CREATE TABLE IF NOT EXISTS temperature (
+	PRIMARY KEY (id),
+    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
+    temperature			FLOAT				NOT NULL,
+    droid_fk			INT					NOT NULL,
+    time_ts				TIMESTAMP			NOT NULL,
+	droid_fk			INT					NOT NULL,
+	FOREIGN KEY (droid_fk)
+		REFERENCES droids(id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS relative_humidity (
+	PRIMARY KEY (id),
+    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
+    relative_humidity	FLOAT				NOT NULL,
+    time_ts				TIMESTAMP			NOT NULL,
+	droid_fk			INT					NOT NULL,
+	FOREIGN KEY (droid_fk)
+		REFERENCES droids(id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS rssi (
+	PRIMARY KEY (id),
+    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
+    rssi				FLOAT				NOT NULL,
+    time_ts				TIMESTAMP			NOT NULL,
+	droid_fk			INT					NOT NULL,
+	FOREIGN KEY (droid_fk)
+		REFERENCES droids(id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS soil_moisture (
 	PRIMARY KEY (id),
     id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
     soil_moisture		FLOAT				NOT NULL,
-	droid_fk			INT					NOT NULL,
-=======
-/* one soil temp reading */
-CREATE TABLE IF NOT EXISTS soil_temperature (
-	PRIMARY KEY (id),
-    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
-    soil_temperature	FLOAT				NOT NULL,
->>>>>>> Stashed changes
     time_ts				TIMESTAMP			NOT NULL,
+	droid_fk			INT					NOT NULL,
 	FOREIGN KEY (droid_fk)
-		REFERENCES profiles(id)
+		REFERENCES droids(id)
         ON UPDATE CASCADE ON DELETE CASCADE
-
 );
 
-/* one soil temp reading */
 CREATE TABLE IF NOT EXISTS soil_temperature (
 	PRIMARY KEY (id),
     id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
     soil_temperature	FLOAT				NOT NULL,
-    droid_fk			INT					NOT NULL,
     time_ts				TIMESTAMP			NOT NULL,
+    droid_fk			INT					NOT NULL,
     FOREIGN KEY (droid_fk)
 		REFERENCES droids(id)
         ON UPDATE CASCADE ON DELETE CASCADE
+
 );
 
-<<<<<<< Updated upstream
-/* one uv sensor reading */
-=======
-/* one soil moisture reading */
-CREATE TABLE IF NOT EXISTS soil_moisture (
-	PRIMARY KEY (id),
-    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
-    soil_moisture		FLOAT				NOT NULL,
-    time_ts				TIMESTAMP			NOT NULL,
-	droid_fk			INT					NOT NULL,
-	FOREIGN KEY (droid_fk)
-		REFERENCES droids(id)
-        ON UPDATE CASCADE ON DELETE CASCADE
-);
-
->>>>>>> Stashed changes
 CREATE TABLE IF NOT EXISTS uv (
 	PRIMARY KEY (id),
     id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
     uv_index			FLOAT				NOT NULL,
     droid_fk			INT					NOT NULL,
     time_ts				TIMESTAMP			NOT NULL,
-<<<<<<< Updated upstream
     FOREIGN KEY (droid_fk)
-		REFERENCES profiles(id)
-        ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-/* one temperature sensor reading */
-CREATE TABLE IF NOT EXISTS temperature (
-	PRIMARY KEY (id),
-    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
-    temperature			FLOAT				NOT NULL,
-    droid_fk			INT					NOT NULL,
-    time_ts				TIMESTAMP			NOT NULL,
-    FOREIGN KEY (droid_fk)
-		REFERENCES profiles(id)
-        ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-/* one relative_humidity sensor reading */
-CREATE TABLE IF NOT EXISTS relative_humidity (
-	PRIMARY KEY (id),
-    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
-    relative_humidity	FLOAT				NOT NULL,
-    droid_fk			INT					NOT NULL,
-    time_ts				TIMESTAMP			NOT NULL,
-    FOREIGN KEY (droid_fk)
-		REFERENCES profiles(id)
-        ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-/* one RSSI sensor reading */
-CREATE TABLE IF NOT EXISTS rssi (
-	PRIMARY KEY (id),
-    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
-    rssi				FLOAT				NOT NULL,
-    droid_fk			INT					NOT NULL,
-    time_ts				TIMESTAMP			NOT NULL,
-    FOREIGN KEY (droid_fk)
-		REFERENCES profiles(id)
-=======
-	droid_fk			INT					NOT NULL,
-	FOREIGN KEY (droid_fk)
-		REFERENCES droids(id)
->>>>>>> Stashed changes
-        ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS rssi (
-	PRIMARY KEY (id),
-    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
-    rssi				FLOAT				NOT NULL,
-    time_ts				TIMESTAMP			NOT NULL,
-	droid_fk			INT					NOT NULL,
-	FOREIGN KEY (droid_fk)
-		REFERENCES droids(id)
-        ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS relative_humidity (
-	PRIMARY KEY (id),
-    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
-    relative_humidity	FLOAT				NOT NULL,
-    time_ts				TIMESTAMP			NOT NULL,
-	droid_fk			INT					NOT NULL,
-	FOREIGN KEY (droid_fk)
-		REFERENCES droids(id)
-        ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS temperature (
-	PRIMARY KEY (id),
-    id					INT					NOT NULL AUTO_INCREMENT UNIQUE,
-    temperature			FLOAT				NOT NULL,
-    time_ts				TIMESTAMP			NOT NULL,
-	droid_fk			INT					NOT NULL,
-	FOREIGN KEY (droid_fk)
 		REFERENCES droids(id)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -174,30 +105,13 @@ desc break_here; /* This is the basic data fro creating a user and the three dro
 INSERT INTO profiles (name, email, subject, userName, region, userPoolId)
 VALUES ('R2D2', 'r2d2@rebellion.com', '12345', '12345', 'tatooine', '12345');
 
-INSERT INTO profiles (name, email, subject, userName, region, userPoolId)
-VALUES ('R2D2', 'r2d2@rebellion.com', '12345', '12345', 'tatooine', '12345');
-
 INSERT INTO droids (droid_id, name, creator_fk)
 VALUES ('0x00b', 'droid1', 1);
 
 INSERT INTO droids (droid_id, name, creator_fk)
-<<<<<<< Updated upstream
-VALUES ('0x00b', 'droid2', 2);
-
-SELECT
-	*
-FROM
-	UV;
-=======
 VALUES ('0x00b', 'droid2', 1);
 
 INSERT INTO droids (droid_id, name, creator_fk)
 VALUES ('0x00b', 'droid3', 1);
 
-
 select * from droids;
-
-
-
-
->>>>>>> Stashed changes
