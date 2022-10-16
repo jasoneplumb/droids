24239b4623d9b92bc57d3094ba377409d4e1dc9c
Jason Plumb <jasoneplumb@gmail.com>
2022-10-15 21:02:51 -0700

    default to new bme280 sensor (instead of the am2320) for humidity and temperature

--- droid00b/relative_humidity.py
+++ droid00b/relative_humidity.py
@@ -1,11 +1,12 @@
 def Reading():
-    USE_BME280 = False
+    USE_BME280 = True
     if (USE_BME280):
         from adafruit_bme280 import basic as adafruit_bme280
         failure_count = 0
         success = False
         while not success:
             try:
+                import board
                 i2c = board.I2C()  # uses board.SCL and board.SDA
                 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
                 result = bme280.humidity
