937af7e3c308ddba8a42fc3e8bb7f1cdc0cbaa3d
Jason E Plumb <jasoneplumb@gmail.com>
2023-01-04 19:27:20 -0800

    improved error handling and text saturation

--- droid00d/flower/image/main.py
+++ droid00d/flower/image/main.py
@@ -7,7 +7,7 @@ import random
 import sys
 import time
 
-SIZE = 0.3
+SIZE = 0.35
 SCALE_FACTOR = 0.7
 TEXT_Y = badger2040.HEIGHT - 5
 PIXELS_PER_IMAGE = 37888 # equals 296 * 128
@@ -26,7 +26,12 @@ def ShowErrorMessageAndExit( msg ):
     return
 
 def BinFilesIn( path ): # returns an array of .bin files in the specified directory
-    return [f for f in os.listdir(path) if f.endswith(".bin")]
+    result = []
+    try:
+        result = [f for f in os.listdir(path) if f.endswith(".bin")]
+    except (OSError, ImportError):
+        pass
+    return result
 
 def ShowImages( a, b, c ): # image file names of the three portions
     display.pen(PEN_WHITE)
@@ -51,7 +56,11 @@ def ShowImages( a, b, c ): # image file names of the three portions
         index = index - 1
         COMPOSITE_IMAGE[index] = A_IMAGE[index] + B_IMAGE[index] + C_IMAGE[index]
     display.image(COMPOSITE_IMAGE)
+    # Saturation of the text (above) is best preseved by doing two partial updated at the higest rate.
+    display.update_speed(badger2040.UPDATE_TURBO)
     display.partial_update(0, 0, 296, 128 - 8) # - 8 leaves space for the name to not be overwritten/cleared
+    display.partial_update(0, 0, 296, 128 - 8) # - 8 leaves space for the name to not be overwritten/cleared
+    display.update_speed(badger2040.UPDATE_NORMAL)
     return
 
 # Show a random set of images and base names every day
@@ -59,17 +68,17 @@ display = badger2040.Badger2040()
 display.led(LED_FULL_BRIGHT) # illuminate the led while active
 # The A image is on the top
 A_IMAGES = BinFilesIn('images/a')
-if (len(A_IMAGES) == 0): ShowErrorMessageAndExit('No .bin found in /images/a/')
+if (len(A_IMAGES) == 0): ShowErrorMessageAndExit('ERROR: No .bin found in /images/a/')
 A_INDEX = random.randint(0,len(A_IMAGES)-1)
 A_PATH = A_IMAGES[A_INDEX]
 # The B image is in the middle
 B_IMAGES = BinFilesIn('images/b')
-if (len(B_IMAGES) == 0): ShowErrorMessageAndExit('No .bin found in /images/b/')
+if (len(B_IMAGES) == 0): ShowErrorMessageAndExit('ERROR: No .bin found in /images/b/')
 B_INDEX = random.randint(0,len(B_IMAGES)-1)
 B_PATH = B_IMAGES[B_INDEX]
 # The C image appears below the other two
 C_IMAGES = BinFilesIn('images/c')
-if (len(C_IMAGES) == 0): ShowErrorMessageAndExit('No .bin found in /images/c/')
+if (len(C_IMAGES) == 0): ShowErrorMessageAndExit('ERROR: No .bin found in /images/c/')
 C_INDEX = random.randint(0,len(C_IMAGES)-1)
 C_PATH = C_IMAGES[C_INDEX]
 ShowImages(A_PATH, B_PATH, C_PATH)
